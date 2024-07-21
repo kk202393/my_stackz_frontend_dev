c08d2e55a44125842eecc79310e510d818378e4e
kk202393 <kailash.lodhi@gmail.com>
2024-07-21 11:18:01 +0530

    update user location file

--- lib/widgets/user_location.dart
+++ lib/widgets/user_location.dart
@@ -15,18 +15,13 @@ class LocationWidget extends StatefulWidget {
 class _LocationWidgetState extends State<LocationWidget> {
   StreamController<void> _streamController = StreamController<void>();
   late Timer _timer;
-  late DateTime _currentTime;
 
   void _getCurrentLocation() async {
     bool serviceEnabled;
     LocationPermission permission;
 
-    // Test if location services are enabled.
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
       return Future.error('Location services are disabled.');
     }
 
@@ -34,23 +29,14 @@ class _LocationWidgetState extends State<LocationWidget> {
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
         return Future.error('Location permissions are denied');
       }
     }
 
     if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
       return Future.error(
           'Location permissions are permanently denied, we cannot request permissions.');
     }
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
     setState(() {
@@ -62,21 +48,16 @@ class _LocationWidgetState extends State<LocationWidget> {
   @override
   void initState() {
     super.initState();
-    _currentTime = DateTime.now();
     _streamController = StreamController<void>();
-
-    // Listen to the stream and update the state
     _streamController.stream.listen((_) {
       _getCurrentLocation();
-      // setState(() {
-      //   _currentTime = DateTime.now();
-      // });
     });
-
-    // Timer to add events to the stream every second
-    _timer = Timer.periodic(Duration(milliseconds: 500), (Timer timer) {
-      _streamController.add(null);
-    });
+    _timer = Timer.periodic(
+      const Duration(milliseconds: 500),
+      (Timer timer) {
+        _streamController.add(null);
+      },
+    );
   }
 
   @override
@@ -93,13 +74,8 @@ class _LocationWidgetState extends State<LocationWidget> {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        // ElevatedButton(
-        //   onPressed: _getCurrentLocation,
-        //   child: Text('Get Location'),
-        // ),
-        SizedBox(height: 20),
+        const SizedBox(height: 20),
         Text(locationMessage),
-        
       ],
     );
   }
